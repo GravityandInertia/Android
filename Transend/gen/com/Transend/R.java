a3c682163a2571b2f22415417dea57bf3cb9066a
Brandon <Infinit.Brandon@gmail.com>
2011-11-05 11:28:54 -0700

    This is an update it has the login screen working, though pressing the login button doesnt do anything as of yet.

--- Transend/gen/com/Transend/R.java
+++ Transend/gen/com/Transend/R.java
@@ -13,8 +13,18 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int driverButton=0x7f050006;
+        public static final int password=0x7f050002;
+        public static final int password_text=0x7f050003;
+        public static final int submitButton=0x7f050004;
+        public static final int userButton=0x7f050005;
+        public static final int username=0x7f050000;
+        public static final int username_text=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
